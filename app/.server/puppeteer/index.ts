ddc85a14d47724d638646895064e129d0dc3e254
YoseptF <yosept.flores@gmail.com>
2024-03-27 23:18:25 -0600

    stealth

--- app/.server/puppeteer/index.ts
+++ app/.server/puppeteer/index.ts
@@ -1,9 +1,12 @@
-import { launch } from "puppeteer";
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+import puppeteer from "puppeteer-extra";
 
-const TIMEOUT = 5000
+puppeteer.use(StealthPlugin())
+
+const TIMEOUT = 150000
 
 // open new browser
-const browser = await launch({
+const browser = await puppeteer.launch({
   headless: false,
   slowMo: 50,
   // window size
@@ -18,33 +21,37 @@ const page = await browser.newPage();
 // go to visa site
 await page.goto("https://ais.usvisa-info.com/en-mx/niv/schedule/56737921/courier");
 
-await page.setDefaultNavigationTimeout(0);
-
 // get button with text ok
 const okButton = await page.waitForSelector('xpath/.//button[text()="OK"]');
 
-if(!okButton) throw new Error("Ok Button not found");
+if (!okButton) throw new Error("Ok Button not found");
 
 okButton.click();
 
 const emailInput = await page.waitForSelector('input#user_email');
 const passwordInput = await page.waitForSelector('input#user_password');
 const signInButton = await page.waitForSelector('input[value="Sign In"]');
-// const policyCheckBox = await page.waitForSelector('input#policy_confirmed');
+const policyCheckBox = await page.waitForSelector('input#policy_confirmed');
 
-if(!emailInput || !passwordInput || !signInButton) throw new Error("Inputs not found");
+if (
+  !emailInput
+  || !passwordInput
+  || !signInButton
+  || !policyCheckBox
+) {
+  console.debug({
+    emailInput: !!emailInput,
+    passwordInput: !!passwordInput,
+    signInButton: !!signInButton,
+    policyCheckBox: !!policyCheckBox,
+  })
+  throw new Error("Inputs not found");
+}
 
-await emailInput.type("email");
-await passwordInput.type("password");
-
-console.debug("emailInput", emailInput);
-console.debug("passwordInput", passwordInput);
-
-
-// check if it needs to sign in
-const signIn = await page.$("button#signIn");
-
-console.debug("signIn", signIn);
+await emailInput.type("testo@yosept.me");
+await passwordInput.type("1_Password");
+await policyCheckBox.click();
+await signInButton.click();
 
 await new Promise((resolve) => setTimeout(resolve, TIMEOUT));
 
