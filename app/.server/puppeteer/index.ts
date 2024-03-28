ce6d686c7ff6216adf869d03ee9f52398e2dfe42
YoseptF <yosept.flores@gmail.com>
2024-03-27 23:00:49 -0600

    remove h captcha

--- app/.server/puppeteer/index.ts
+++ app/.server/puppeteer/index.ts
@@ -1,12 +1,11 @@
-import { hcaptcha } from "puppeteer-hcaptcha";
 import { launch } from "puppeteer";
 
-const TIMEOUT = 150000
+const TIMEOUT = 5000
 
 // open new browser
 const browser = await launch({
   headless: false,
-  // slowMo: 50,
+  slowMo: 50,
   // window size
   defaultViewport: {
     width: 700,
@@ -21,12 +20,8 @@ await page.goto("https://ais.usvisa-info.com/en-mx/niv/schedule/56737921/courier
 
 await page.setDefaultNavigationTimeout(0);
 
-await hcaptcha(page);
-
 // get button with text ok
 const okButton = await page.waitForSelector('xpath/.//button[text()="OK"]');
-
-
 
 if(!okButton) throw new Error("Ok Button not found");
 
@@ -35,14 +30,16 @@ okButton.click();
 const emailInput = await page.waitForSelector('input#user_email');
 const passwordInput = await page.waitForSelector('input#user_password');
 const signInButton = await page.waitForSelector('input[value="Sign In"]');
-const policyCheckBox = await page.waitForSelector('input#policy_confirmed');
+// const policyCheckBox = await page.waitForSelector('input#policy_confirmed');
 
 if(!emailInput || !passwordInput || !signInButton) throw new Error("Inputs not found");
 
 await emailInput.type("email");
 await passwordInput.type("password");
 
-// await signInButton.click();
+console.debug("emailInput", emailInput);
+console.debug("passwordInput", passwordInput);
+
 
 // check if it needs to sign in
 const signIn = await page.$("button#signIn");
